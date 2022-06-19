dfe18b163b91b32af442cbfd816e37c924de881a
Ajith <ajithpallisseryantony@gmail.com>
2022-06-19 13:58:38 +0400

    form block

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,6 +1,35 @@
+import styles from "./styles.module.scss";
 const FormBlock = () => {
-    return <div>FormBlock</div>
-    }
-    
-    export default FormBlock
-    
+  return (
+    <div className={styles.formBlock}>
+      <div className={styles.headingSection}>
+        <h3>Receive payments quickly from anywhere</h3>
+        <h4>
+          Why kept very ever home mrs. Considered sympathize ten uncommonly
+          occasional assistance sufficient not. Letter of on become he tended
+          active enable to.
+        </h4>
+      </div>
+      <form>
+        <div className={styles.form}>
+          <h4>Get Started for Free</h4>
+          <div className={styles.formInput}>
+            <label>Name</label>
+            <input placeholder="Name" />
+          </div>
+          <div className={styles.formInput}>
+            <label>Email Address</label>
+            <input placeholder="Email Address" />
+          </div>
+          <div className={styles.formInput}>
+            <label>Password</label>
+            <input placeholder="Password" />
+          </div>
+          <button>GET STARTED</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormBlock;
